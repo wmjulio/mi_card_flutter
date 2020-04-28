746e8b4bec09d440bdf417a6b80d913b9b682108
Julio Martins Filho <wmjulio@gmail.com>
2020-04-28 00:09:19 -0300

    [DONE] L6 - Flutter Card

--- lib/main.dart
+++ lib/main.dart
@@ -14,37 +14,85 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.deepOrange,
-              ),
-              Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      color: Colors.yellowAccent,
-                        width: 100,
-                        height: 100,
-                    ),
-                    Container(
-                      color: Color.fromARGB(50, 255, 255, 0),
-                      width: 100,
-                      height: 100,
-                    ),
-                  ],
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 65.0,
+                  backgroundColor: Colors.red,
+                  backgroundImage: AssetImage('images/julioMartins.jpg'),
                 ),
-              ),
-              Container(
-                width: 100.0,
-                color: Colors.blueGrey,
-              ),
-            ],
+                Text(
+                  'J. Martins',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 30.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      color: Colors.teal[100],
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(
+                  width: 150,
+                  height: 20,
+                  child: Divider(
+                    color: Colors.teal[100],
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 25.0
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone_android,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+55 83 9 96836652',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.only(
+                      left: 25.0,
+                      right: 25.0,
+                      bottom: 10.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                        Icons.mail_outline,
+                        color: Colors.teal,
+                      ),
+                    title: Text(
+                        'wmjulio@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                        ),
+                      )
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
